7f95bd02a67506f24c1f836cf94d2004cd1f714e
maxlune <85277579+maxlune@users.noreply.github.com>
2023-01-13 12:10:20 +0100

    css layout

--- src/components/MyRatingComponent/MyRatingComponent.tsx
+++ src/components/MyRatingComponent/MyRatingComponent.tsx
@@ -4,16 +4,25 @@ import { RatingButtonProps } from '../RatingButton/RatingButton';
 import RatingContent from '../RatingContent';
 import { RatingContentProps } from '../RatingContent/RatingContent';
 import RatingRadio from '../RatingRadio';
+import './MyRatingComponent.css';
 
 type RatingProps = RatingContentProps & RatingButtonProps;
 
 const MyRatingComponent = ({contentTitle, contentText, labelButton}: RatingProps) => {
   return (
     <div className='rating-card'>
-      <img src="" alt="" />
-      <RatingContent contentTitle={contentTitle} contentText={contentText} />
-      <RatingRadio />
-      <RatingButton labelButton={labelButton} />
+      <div className='icon-star'>
+        <img src="src\assets\images\icon-star.svg" alt="icon-star" />
+      </div>
+      <div className='rating-content'>
+        <RatingContent contentTitle={contentTitle} contentText={contentText} />
+      </div>
+      <div className='rating-radio'>
+        <RatingRadio />
+      </div>
+      <div className='rating-button'>
+        <RatingButton labelButton={labelButton} />
+      </div>
     </div>
   )
 }
